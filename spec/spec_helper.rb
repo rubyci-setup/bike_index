844594c13cea6056d90c0b7a24e5515e2e7b66f2
rubyci-setup <rubyci.setup@gmail.com>
2024-12-10 15:45:02 +0100

    trigger new build 1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,20 @@
 if ENV["FAST_CI_SECRET_KEY"]
-    require "fast_ci"
-    require "rspec/core/runner"
-    require "fast_ci/runner_prepend"
+  require "fast_ci"
+  require "rspec/core/runner"
+  require "fast_ci/runner_prepend"
 
-    class RSpec::Core::ExampleGroup
-      def self.filtered_examples
-        ids = Thread.current[:fastci_scoped_ids] || ""
+  class RSpec::Core::ExampleGroup
+    def self.filtered_examples
+      ids = Thread.current[:fastci_scoped_ids] || ""
 
-        RSpec.world.filtered_examples[self].filter do |ex|
-          ids == "" || /^#{ids}($|:)/.match?(ex.metadata[:scoped_id])
-        end
+      RSpec.world.filtered_examples[self].filter do |ex|
+        ids == "" || /^#{ids}($|:)/.match?(ex.metadata[:scoped_id])
       end
     end
-
-    RSpec::Core::Runner.prepend(FastCI::RunnerPrepend)
   end
+
+  RSpec::Core::Runner.prepend(FastCI::RunnerPrepend)
+end
 RSpec.configure do |config|
   # Use color in STDOUT
   config.color = true
